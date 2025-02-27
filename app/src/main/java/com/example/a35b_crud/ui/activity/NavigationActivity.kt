4edc3cae3902bf96c58748a7867d7f1147291ed5
Shreeya Paudel <shreeya.paudel.8e@gmail.com>
2025-02-27 23:10:10 +0545

    login, signup

---
+++ app/src/main/java/com/example/a35b_crud/ui/activity/NavigationActivity.kt
@@ -0,0 +1,70 @@
+package com.example.a35b_crud.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.a35b_crud.R
+import com.example.a35b_crud.databinding.ActivityNavigationBinding
+import com.example.a35b_crud.ui.fragment.CartFragment
+import com.example.a35b_crud.ui.fragment.HomeFragment
+import com.example.a35b_crud.ui.fragment.ProfileFragment
+import com.example.a35b_crud.utils.LoadingUtils
+
+class NavigationActivity : AppCompatActivity() {
+    lateinit var navigationBinding: ActivityNavigationBinding
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        navigationBinding = ActivityNavigationBinding.inflate(layoutInflater)
+
+        setContentView(navigationBinding.root)
+
+
+        replaceFragment(HomeFragment())
+
+
+        navigationBinding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+
+                R.id.menuHome -> replaceFragment(HomeFragment())
+                R.id.menuCart -> {
+
+
+                    val intent = Intent(this, ProductDashboardActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menuProfile -> replaceFragment(ProfileFragment())
+                else -> {}
+            }
+            true
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/a35b_crud/ui/activity/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.example.a35b_crud.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a35b_crud.R
+import com.example.a35b_crud.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
